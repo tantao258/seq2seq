831f9fad54ccc5eb592978728293ce7b406c950b
tantao258 <823232162@qq.com>
2018-07-04 16:59:55 +0800

    updata

--- Attention/HAN_text_classification/HAN_model.py
+++ Attention/HAN_text_classification/HAN_model.py
@@ -226,10 +226,12 @@ class HAN():
         #inputs是GRU的输出，size是[batch_size, max_time, encoder_size(hidden_size * 2)]
         with tf.variable_scope(name):
             # u_context是上下文的重要性向量，用于区分不同单词/句子对于句子/文档的重要程度,
-            # 因为使用双向GRU，所以其长度为2×hidden_szie
+            # 因为使用双向GRU，所以其长度为 (2×hidden_size,)
             u_context = tf.Variable(tf.truncated_normal([self.hidden_size * 2]), name='u_context')
-            #使用一个全连接层编码GRU的输出的到期隐层表示,输出u的size是[batch_size, max_time, hidden_size * 2]
+
+            #使用一个全连接层编码GRU的输出的到其隐层表示,输出h的size是[batch_size, max_time, hidden_size * 2]
             h = layers.fully_connected(inputs, self.hidden_size * 2, activation_fn=tf.nn.tanh)
+
             #shape为[batch_size, max_time, 1]
             alpha = tf.nn.softmax(tf.reduce_sum(tf.multiply(h, u_context), axis=2, keep_dims=True), dim=1)
             #reduce_sum之前shape为[batch_szie, max_time, hidden_szie*2]，之后shape为[batch_size, hidden_size*2]
